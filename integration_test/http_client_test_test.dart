5c6e82d05b2beaadba8e3762a7d14951ef31683f
Björn Götz <bjoern.goetz2001@gmx.de>
2023-05-15 00:00:19 +0200

    feat: add 'matchesRequestSnapshot' matcher

---
+++ integration_test/http_client_test_test.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:http_client_test/http_client_test.dart';
+
+void main() {
+  group('http_client_test', () {
+    group('matchesRequestSnapshot', () {
+      Future<void> Function(Uri) sendRequest({
+        final String body = 'RequestBody',
+      }) =>
+          (endpoint) => HttpClient()
+              .post(endpoint.host, endpoint.port, '/request/path?key=value')
+              .then((request) => request..write(body))
+              .then((request) => request.close());
+
+      Future<bool> isFailed(Future<void> Function() function) async {
+        bool isFailed = false;
+
+        try {
+          await function();
+        } catch (e) {
+          isFailed = true;
+        }
+
+        return isFailed;
+      }
+
+      test('should succeed when request and snapshot are equal', () async {
+        final result = await isFailed(() => expectLater(
+              sendRequest(),
+              matchesRequestSnapshot(
+                'integration_test/matchesRequestSnapshot.http',
+              ),
+            ));
+
+        expect(result, isFalse);
+      });
+
+      test('should fail when request and snapshot aren\'t equal', () async {
+        final result = await isFailed(() => expectLater(
+              sendRequest(body: 'DifferentBody'),
+              matchesRequestSnapshot(
+                'integration_test/matchesRequestSnapshot.http',
+              ),
+            ));
+
+        expect(result, isTrue);
+      });
+
+      test('should fail when snapshot file doesn\'t exist', () async {
+        final result = await isFailed(() => expectLater(
+              sendRequest(),
+              matchesRequestSnapshot('unknown_snapshot.http'),
+            ));
+
+        expect(result, isTrue);
+      });
+    });
+  });
+}
--- lib/http_client_test.dart
+++ lib/http_client_test.dart
@@ -1,1 +1,15 @@
+import 'package:http_client_test/src/captor/mock_http_server_impl.dart';
+import 'package:http_client_test/src/captor/request_captor_impl.dart';
+import 'package:http_client_test/src/captor/request_serializer_impl.dart';
+import 'package:http_client_test/src/request_snapshot_comparator_impl.dart';
+import 'package:http_client_test/src/loader/file_snapshot_loader.dart';
+import 'package:http_client_test/src/matcher/request_snapshot_matcher.dart';
+import 'package:matcher/src/expect/async_matcher.dart';
 
+AsyncMatcher matchesRequestSnapshot(final String snapshotPath) =>
+    RequestSnapshotMatcher(
+      RequestSnapshotComparatorImpl(
+        RequestCaptorImpl(MockHttpServerImpl(RequestSerializerImpl())),
+        FileSnapshotLoader(snapshotPath),
+      ),
+    );
