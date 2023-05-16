a3a0b9817f235ea03207c3366009f29afc44f022
Björn Götz <bjoern.goetz2001@gmx.de>
2023-05-16 21:16:03 +0200

    feat: add 'captureResponseSnapshot' function

--- integration_test/http_client_test_test.dart
+++ integration_test/http_client_test_test.dart
@@ -1,19 +1,28 @@
 import 'dart:io';
 
+import 'package:http_client_test/src/captor/mock_http_server_impl.dart';
+import 'package:http_client_test/src/captor/request_captor.dart';
+import 'package:http_client_test/src/captor/request_captor_impl.dart';
+import 'package:http_client_test/src/snapshot/request_snapshot.dart';
+import 'package:http_client_test/src/snapshot/response_snapshot.dart';
 import 'package:test/test.dart';
 import 'package:http_client_test/http_client_test.dart';
 
 void main() {
   group('http_client_test', () {
-    group('matchesRequestSnapshot', () {
-      Future<void> Function(Uri) sendRequest({
-        final String body = 'RequestBody',
-      }) =>
-          (endpoint) => HttpClient()
-              .post(endpoint.host, endpoint.port, '/request/path?key=value')
-              .then((request) => request..write(body))
-              .then((request) => request.close());
+    Future<void> Function(Uri) sendRequest({
+      final String body = 'RequestBody',
+    }) =>
+        (endpoint) => HttpClient()
+            .post(endpoint.host, endpoint.port, '/request/path?key=value')
+            .then((request) {
+              request.headers.add('authorization', 'Bearer token');
+              return request;
+            })
+            .then((request) => request..write(body))
+            .then((request) => request.close());
 
+    group('matchesRequestSnapshot', () {
       Future<bool> isFailed(Future<void> Function() function) async {
         bool isFailed = false;
 
@@ -69,6 +78,55 @@ void main() {
         final result = await run(captureFileName);
 
         expect(result, isFalse);
+      });
+    });
+
+    group('captureResponseSnapshot', () {
+      Future<RequestSnapshot?> mockServer(final RequestSender sendRequest) =>
+          RequestCaptorImpl(
+            MockHttpServerImpl(
+              port: 8081,
+              response: ResponseSnapshot(
+                statusCode: 200,
+                headers: {
+                  'content-type': 'text/plain',
+                },
+                body: 'ResponseBody',
+              ),
+            ),
+          ).capture(sendRequest);
+
+      Future<String> readFile(final String path) => File(path).readAsString();
+
+      test('should save response capture', () async {
+        const snapshotFile = 'integration_test/captureResponseSnapshot.http';
+
+        await mockServer(
+          (endpoint) => captureResponseSnapshot(
+            snapshotFile: snapshotFile,
+            endpoint: endpoint,
+            sendRequest: sendRequest(),
+          ),
+        );
+
+        final actual = await readFile('$snapshotFile.capture');
+        final expected = await readFile(snapshotFile);
+        expect(actual, equals(expected));
+      });
+
+      test('should send request to real endpoint', () async {
+        final request = await mockServer(
+          (endpoint) => captureResponseSnapshot(
+            snapshotFile: 'unknown_snapshot.http',
+            endpoint: endpoint,
+            sendRequest: sendRequest(),
+          ),
+        );
+
+        expect(request?.method, equals('POST'));
+        expect(request?.path, equals('/request/path?key=value'));
+        expect(request?.headers['authorization'], equals('Bearer token'));
+        expect(request?.body, equals('RequestBody'));
       });
     });
   });
--- lib/http_client_test.dart
+++ lib/http_client_test.dart
@@ -1,30 +1,47 @@
 import 'package:http_client_test/src/captor/mock_http_server_impl.dart';
+import 'package:http_client_test/src/captor/request_captor.dart';
 import 'package:http_client_test/src/captor/request_captor_impl.dart';
+import 'package:http_client_test/src/captor/snapshot_http_client_impl.dart';
 import 'package:http_client_test/src/loader/request_snapshot_serializer_impl.dart';
 import 'package:http_client_test/src/loader/response_snapshot_serializer_impl.dart';
+import 'package:http_client_test/src/loader/snapshot_loader.dart';
 import 'package:http_client_test/src/request_snapshot_comparator_impl.dart';
 import 'package:http_client_test/src/loader/file_snapshot_loader.dart';
 import 'package:http_client_test/src/matcher/request_snapshot_matcher.dart';
 import 'package:http_client_test/src/snapshot/response_snapshot.dart';
 import 'package:test/test.dart';
 
-Matcher matchesRequestSnapshot(final String snapshotPath) =>
+Matcher matchesRequestSnapshot(final String snapshotFile) =>
     RequestSnapshotMatcher(
       RequestSnapshotComparatorImpl(
-        RequestCaptorImpl(
-          MockHttpServerImpl(
-            port: 8080,
-            response: ResponseSnapshot(
-              statusCode: 200,
-              headers: {},
-              body: '',
-            ),
-          ),
-        ),
-        FileSnapshotLoader(
-          requestSerializer: RequestSnapshotSerializerImpl(),
-          responseSerializer: ResponseSnapshotSerializerImpl(),
-          path: snapshotPath,
-        ),
+        _requestCaptor,
+        _newLoader(snapshotFile),
       ),
+    );
+
+Future<void> captureResponseSnapshot({
+  required final String snapshotFile,
+  required final Uri endpoint,
+  required final RequestSender sendRequest,
+}) async {
+  final request = await _requestCaptor.capture(sendRequest);
+  final response = await SnapshotHttpClientImpl().send(endpoint, request!);
+  await _newLoader(snapshotFile).saveResponseCapture(response);
+}
+
+final _requestCaptor = RequestCaptorImpl(
+  MockHttpServerImpl(
+    port: 8080,
+    response: ResponseSnapshot(
+      statusCode: 200,
+      headers: {},
+      body: '',
+    ),
+  ),
+);
+
+SnapshotLoader _newLoader(final String snapshotFile) => FileSnapshotLoader(
+      requestSerializer: RequestSnapshotSerializerImpl(),
+      responseSerializer: ResponseSnapshotSerializerImpl(),
+      path: snapshotFile,
     );
