619989baca86094c4256e194e18b8ff37a94da43
Björn Götz <bjoern.goetz2001@gmx.de>
2023-05-18 18:45:26 +0200

    refactor: rename example files

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -2,15 +2,15 @@ import 'package:test/test.dart';
 import 'package:http_client_test/http_client_test.dart';
 
 // ignore: avoid_relative_lib_imports
-import '../lib/client.dart';
+import '../lib/example.dart';
 
 void main() {
   group('UserClient', () {
     test('getById', () async {
       final result = await matchesRequestSnapshot(
         send: (endpoint) => UserClient(endpoint).getById(12),
-        request: 'example/test/client_test.request.http',
-        response: 'example/test/client_test.response.http',
+        request: 'example/test/example_test.request.http',
+        response: 'example/test/example_test.response.http',
       );
 
       expect(result.id, equals(12));
